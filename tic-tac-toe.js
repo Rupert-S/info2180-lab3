9e3d7a0f6d5110c86a705aaef85d5d92a63837fe
Rupert Scott <rupertscott1706@gmail.com>
2021-10-08 13:00:42 -0500

    Alternate X or O

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,9 +3,29 @@ function windowLoad(){
    // insert squares HTML in DOM
   var blocks = document.getElementById("board").childNodes;
   blocks.forEach(element => { element.className = "square";});
+  
+  const x = 'X'
+  const circ = 'O'
+  let circTurn
+
+blocks.forEach(elem => {elem.addEventListener('click', isClick)
+})
+function isClick(e){
+  const space = e.target
+  const currentClass = circTurn ? circ : x
+  spaceClass(space, currentClass)
+  changeClass()
 }
 
+function spaceClass(space, currentClass){
+  space.textContent = currentClass;
+  console.log('button clicked');
+  space.classList.add(currentClass)
+}
 
-
+function changeClass(){
+  circTurn = !circTurn
+}
+}
 
 
