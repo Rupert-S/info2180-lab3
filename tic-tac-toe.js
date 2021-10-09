12995b8965a5d233850a9354ea3e9a41c7ef7f33
Rupert Scott <rupertscott1706@gmail.com>
2021-10-09 18:40:35 -0500

    Ways to win

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -6,6 +6,16 @@ function windowLoad(){
   
   const x = 'X'
   const circ = 'O'
+  const WINNING_WAYS = [
+    [0,1,2], 
+    [3,4,5], 
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8], 
+    [2,4,6]
+  ];
   let circTurn
 
 blocks.forEach(elem => {elem.addEventListener('click', isClick)
@@ -15,6 +25,7 @@ function isClick(e){
   const currentClass = circTurn ? circ : x
   spaceClass(space, currentClass)
   changeClass()
+  whoWon()
 }
 
 function spaceClass(space, currentClass){
@@ -26,6 +37,7 @@ function spaceClass(space, currentClass){
 function changeClass(){
   circTurn = !circTurn
 }
+
 blocks.forEach(function(elem, index, list) { 
   elem.addEventListener('mouseover',function(e){
       e.target.classList.add('hover');
@@ -35,6 +47,30 @@ blocks.forEach(function(elem, index, list) {
       e.target.classList.remove('hover');
    });
 });
+
+//checking for a winner
+ function whoWon(){
+  let roundWon = false;
+  let gameState = [];
+  for (let i = 0; i<= 7; i++){
+    const winCond = WINNING_WAYS[i];
+    const a = blocks[winCond[0]];
+    const b = blocks[winCond[1]];
+    const c = blocks[winCond[2]];
+    gameState += [a.textContent, b.textContent, c.textContent]
+    if (a.textContent == '' || b.textContent =='' || c.textContent ==''){
+      continue;
+    }
+    if(a.textContent != '' && a.textContent == b.textContent && b.textContent == c.textContent && a.textContent == c.textContent){
+      roundWon = true;
+      console.log(gameState);
+      break;
+    }
+  }
+  if(roundWon){
+    console.log('winner') 
+  }
+}
 }
 
 
